c8bc88c4d8669ddeb46779d8002dc66b66dbda76
Ultoman <jabonasera@gmail.com>
2020-11-17 17:05:44 -0600

    Formatting

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from "react";
-import { useHistory } from "react-router-dom";
-import UserContext from "../../context/userContext";
-import Axios from "axios";
-import ErrorNotice from "../misc/ErrorNotice";
+import React, { useState, useContext } from 'react';
+import { useHistory } from 'react-router-dom';
+import UserContext from '../../context/userContext';
+import Axios from 'axios';
+import ErrorNotice from '../misc/ErrorNotice';
 
 export default function Register() {
   const [email, setEmail] = useState();
@@ -19,56 +19,56 @@ export default function Register() {
 
     try {
       const newUser = { email, password, name: displayName };
-      await Axios.post("http://localhost:5000/api/users/register", newUser);
+      await Axios.post('http://localhost:5000/api/users/register', newUser);
       const loginRes = await Axios.post(
-        "http://localhost:5000/api/users/login",
+        'http://localhost:5000/api/users/login',
         {
           email,
           password,
         }
       );
       setUser(loginRes?.data?.user);
-      localStorage.setItem("auth-token", loginRes.data.token);
-      history.push("/");
+      localStorage.setItem('auth-token', loginRes.data.token);
+      history.push('/');
     } catch (err) {
       err.response.data.msg && setError(err.response.data.msg);
     }
   };
 
   return (
-    <div className="page">
+    <div className='page'>
       <h2>Register</h2>
       {error && (
         <ErrorNotice message={error} clearError={() => setError(undefined)} />
       )}
-      <form className="form" onSubmit={submit}>
-        <label htmlFor="register-email">Email</label>
+      <form className='form' onSubmit={submit}>
+        <label htmlFor='register-email'>Email</label>
         <input
-          id="register-email"
-          type="email"
+          id='register-email'
+          type='email'
           onChange={(e) => setEmail(e.target.value)}
         />
 
-        <label htmlFor="register-password">Password</label>
+        <label htmlFor='register-password'>Password</label>
         <input
-          id="register-password"
-          type="password"
+          id='register-password'
+          type='password'
           onChange={(e) => setPassword(e.target.value)}
         />
         <input
-          type="password"
-          placeholder="Verify password"
+          type='password'
+          placeholder='Verify password'
           onChange={(e) => setPasswordCheck(e.target.value)}
         />
 
-        <label htmlFor="register-display-name">Display name</label>
+        <label htmlFor='register-display-name'>Display name</label>
         <input
-          id="register-display-name"
-          type="text"
+          id='register-display-name'
+          type='text'
           onChange={(e) => setDisplayName(e.target.value)}
         />
 
-        <input type="submit" value="Register" className="primaryButton" />
+        <input type='submit' value='Register' className='primaryButton' />
       </form>
     </div>
   );
