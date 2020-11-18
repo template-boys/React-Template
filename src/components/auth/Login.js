3f7a38e534a5f09754f730a0f4b06060ea9ece43
Ultoman <Ultoman@users.noreply.github.com>
2020-11-18 23:04:52 +0000

    Prettified Code!

--- src/components/auth/Login.js
+++ src/components/auth/Login.js
@@ -1,8 +1,8 @@
-import React, { useState, useContext } from "react";
-import { Link, useHistory } from "react-router-dom";
-import UserContext from "../../context/userContext";
-import Axios from "axios";
-import ErrorNotice from "../misc/ErrorNotice";
+import React, { useState, useContext } from 'react';
+import { Link, useHistory } from 'react-router-dom';
+import UserContext from '../../context/userContext';
+import Axios from 'axios';
+import ErrorNotice from '../misc/ErrorNotice';
 
 export default function Login() {
   const [email, setEmail] = useState();
@@ -17,43 +17,43 @@ export default function Login() {
     try {
       const loginUser = { email, password };
       const loginRes = await Axios.post(
-        "http://localhost:5000/api/users/login",
+        'http://localhost:5000/api/users/login',
         loginUser
       );
       setUser(loginRes.data.user);
-      localStorage.setItem("auth-token", loginRes.data.token);
-      history.push("/");
+      localStorage.setItem('auth-token', loginRes.data.token);
+      history.push('/');
     } catch (err) {
       const errorMessage =
         err?.response?.data?.msg ??
-        "Sorry, something went wrong. Please try again later.";
+        'Sorry, something went wrong. Please try again later.';
       setError(errorMessage);
     }
   };
   return (
-    <div className="page">
+    <div className='page'>
       <h2>Welcome Back</h2>
       {error && (
         <ErrorNotice message={error} clearError={() => setError(undefined)} />
       )}
-      <form className="form" onSubmit={submit}>
-        <label htmlFor="login-email">Email</label>
+      <form className='form' onSubmit={submit}>
+        <label htmlFor='login-email'>Email</label>
         <input
-          id="login-email"
-          type="email"
+          id='login-email'
+          type='email'
           onChange={(e) => setEmail(e.target.value)}
         />
 
-        <label htmlFor="login-password">Password</label>
+        <label htmlFor='login-password'>Password</label>
         <input
-          id="login-password"
-          type="password"
+          id='login-password'
+          type='password'
           onChange={(e) => setPassword(e.target.value)}
         />
 
-        <Link to="/password_reset">Forgotten Password</Link>
+        <Link to='/password_reset'>Forgotten Password</Link>
 
-        <input type="submit" value="Log in" className="primaryButton" />
+        <input type='submit' value='Log in' className='primaryButton' />
       </form>
     </div>
   );
--- src/components/auth/authUtil.js
+++ src/components/auth/authUtil.js
@@ -1,15 +1,15 @@
-import Axios from "axios";
+import Axios from 'axios';
 
 export async function checkForLogin() {
-  let token = localStorage.getItem("auth-token");
+  let token = localStorage.getItem('auth-token');
   if (token === null) {
-    localStorage.setItem("auth-token", "");
-    token = "";
+    localStorage.setItem('auth-token', '');
+    token = '';
   }
   const tokenRes = await Axios.post(
-    "http://localhost:5000/api/users/ping",
+    'http://localhost:5000/api/users/ping',
     null,
-    { headers: { "x-auth-token": token } }
+    { headers: { 'x-auth-token': token } }
   );
   return tokenRes?.data?.user ?? null;
 }
