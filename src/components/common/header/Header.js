0f802bfcd5f827d61c4652461f3262eaa9fc0fdb
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2020-11-23 06:33:53 -0600

    add .scss , theming, and restructuring

--- src/components/common/header/Header.js
+++ src/components/common/header/Header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
-import AuthOptions from '../auth/AuthOptions';
-import pear from './pear.png';
+import AuthOptions from '../../auth/AuthOptions';
+import pear from '../../../pear.png';
+import { useSelector } from 'react-redux';
 
 export default function Header() {
-  return (
+  const user = useSelector((state) => state.userReducer.user);
+  return !!user ? (
     <header id='header'>
       <Link to='/'>
         <h1 className='title'>
@@ -13,5 +15,5 @@ export default function Header() {
       </Link>
       <AuthOptions />
     </header>
-  );
+  ) : null;
 }
