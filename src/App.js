2f8a58d3982b16e1eb107c27148bd96dd8d15b5f
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2020-11-24 16:17:04 -0600

    create index file for all pages

--- src/App.js
+++ src/App.js
@@ -1,20 +1,21 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect } from 'react';
 import { Switch, Route, useHistory } from 'react-router-dom';
+import { useDispatch } from 'react-redux';
 
 import Header from './components/common/header/Header';
-import Home from './components/pages/Home';
-import Login from './components/pages/Login';
-import Register from './components/pages/Register';
-import NotFound from './components/pages/NotFound';
+import {
+  Home,
+  Login,
+  Register,
+  NotFound,
+  UserVerified,
+} from './components/pages/index';
 import PasswordReset from './components/auth/PasswordReset';
 import * as authUtil from './components/auth/utils/authUtils';
 import { setUser } from './redux/User/user.actions';
-
 import './style.scss';
-import UserVerified from './components/pages/UserVerified';
 import PrivateRoute from './components/auth/PrivateRoute';
 import PublicRoute from './components/auth/PublicRoute';
-import { useDispatch } from 'react-redux';
 
 export default function App() {
   const history = useHistory();
