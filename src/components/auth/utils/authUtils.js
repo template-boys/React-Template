5bbcf719c4734414601627190ae128e57ac6960d
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2020-11-24 19:25:34 -0600

    dont ping if no token in local storage

--- src/components/auth/utils/authUtils.js
+++ src/components/auth/utils/authUtils.js
@@ -2,14 +2,16 @@ import Axios from 'axios';
 
 export async function checkForLogin() {
   let token = localStorage.getItem('auth-token');
+  let tokenResponse;
   if (token === null) {
     localStorage.setItem('auth-token', '');
     token = '';
+  } else {
+    tokenResponse = await Axios.post(
+      'http://localhost:5000/api/users/ping',
+      null,
+      { headers: { 'x-auth-token': token } }
+    );
   }
-  const tokenRes = await Axios.post(
-    'http://localhost:5000/api/users/ping',
-    null,
-    { headers: { 'x-auth-token': token } }
-  );
-  return tokenRes?.data?.user ?? null;
+  return tokenResponse?.data?.user;
 }
