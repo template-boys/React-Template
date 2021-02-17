791131b6ebf968526ebf613427f99b1df77045ca
justin-dupre <42753529+justin-dupre@users.noreply.github.com>
2021-02-17 01:36:15 -0600

    i frame

--- src/components/auth/LoginForm.js
+++ src/components/auth/LoginForm.js
@@ -60,7 +60,7 @@ export default function Login() {
         />
       </Form>
       <iframe
-        src='https://clips.twitch.tv/embed?clip=MuddyAwkwardMuleWoofer-itDtGRwR3oIK1gCP&parent=www.react-redux-temp.herokuapp.com/login'
+        src='https://clips.twitch.tv/embed?clip=MuddyAwkwardMuleWoofer-itDtGRwR3oIK1gCP&parent=react-redux-temp.herokuapp.com/login'
         frameborder='0'
         allowfullscreen='true'
         scrolling='no'
